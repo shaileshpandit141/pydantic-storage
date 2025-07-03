3a38409f5a0c16571be57471cedce551c4163813
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-03 10:49:29 +0530

    feat: add type hints for json file.

--- src/json_file_storage/models/typed/meta_data_dict.py
+++ src/json_file_storage/models/typed/meta_data_dict.py
@@ -1,4 +1,24 @@
-from typing import TypedDict
+from typing import Any, TypedDict, TypeVar
+
+from pydantic import BaseModel
+
+T = TypeVar("T", bound=BaseModel)
 
 
-class MetaDataDict(TypedDict): ...
+class TimestampDict(TypedDict):
+    created_at: str
+    updated_at: str
+
+
+class StorageDict(TypedDict):
+    type: str
+    encryption: str
+
+
+class MetaDataDict(TypedDict):
+    version: str
+    title: str
+    description: str
+    timestamps: TimestampDict
+    storage: StorageDict
+    records: dict[str, Any]
