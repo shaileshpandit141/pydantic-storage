88618b3742d50d6ca07a453bc5ac70a625bae5f5
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-04 15:43:52 +0530

    refactor: rename json decoder error exception.

--- src/json_file_storage/exceptions/__init__.py
+++ src/json_file_storage/exceptions/__init__.py
@@ -1,11 +1,11 @@
 from ._duplicate_entry_error import DuplicateEntryError
 from ._json_decode_error import JSONDecodeError
 from ._validation_error import ValidationError
-from .json_encoder_error import JSONEncoderError
+from .json_encode_error import JSONEncodeError
 
 __all__: list[str] = [
     "DuplicateEntryError",
     "JSONDecodeError",
-    "JSONEncoderError",
+    "JSONEncodeError",
     "ValidationError",
 ]
