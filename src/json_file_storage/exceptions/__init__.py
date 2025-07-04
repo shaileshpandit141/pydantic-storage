d2f10cd6b73d57c8150e23dfbd41051a77acc2d4
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-04 15:37:42 +0530

    feat: add json encoder exception.

--- src/json_file_storage/exceptions/__init__.py
+++ src/json_file_storage/exceptions/__init__.py
@@ -1,5 +1,11 @@
 from ._duplicate_entry_error import DuplicateEntryError
 from ._json_decode_error import JSONDecodeError
 from ._validation_error import ValidationError
+from .json_encoder_error import JSONEncoderError
 
-__all__: list[str] = ["DuplicateEntryError", "JSONDecodeError", "ValidationError"]
+__all__: list[str] = [
+    "DuplicateEntryError",
+    "JSONDecodeError",
+    "JSONEncoderError",
+    "ValidationError",
+]
