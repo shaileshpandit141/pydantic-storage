e0be952e503a230cfd0946522ca6ceb6ad977b32
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-06 20:03:40 +0530

    feat: add abstract class for pydantic model serialization.

--- src/json_file_storage/_abstractions/__init__.py
+++ src/json_file_storage/_abstractions/__init__.py
@@ -1,4 +1,9 @@
 from ._abstract_file_manager import AbstractFileManager
 from ._abstract_file_storage import AbstractFileStorage
+from ._abstract_pydantic_model_serializer import AbstractPydanticModelSerializer
 
-__all__: list[str] = ["AbstractFileManager", "AbstractFileStorage"]
+__all__: list[str] = [
+    "AbstractFileManager",
+    "AbstractFileStorage",
+    "AbstractPydanticModelSerializer",
+]
