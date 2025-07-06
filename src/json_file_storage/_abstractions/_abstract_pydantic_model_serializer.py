21546178378c87e0ca53af0421c39a0c7ccb4c9b
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-06 20:21:53 +0530

    fix: update currect type hints.

--- src/json_file_storage/_abstractions/_abstract_pydantic_model_serializer.py
+++ src/json_file_storage/_abstractions/_abstract_pydantic_model_serializer.py
@@ -1,34 +1,34 @@
 from abc import ABC, abstractmethod
 from typing import Any, Generic
 
-from json_file_storage.models.typed import T
+from json_file_storage.models.typed import RecordDict, T
 
 
 class AbstractPydanticModelSerializer(ABC, Generic[T]):
     """Abstract base class for data serializers that manage data serialization and deserialization."""
 
-    def __init__(self, data: dict[str, T]) -> None:
+    def __init__(self, data: RecordDict[T]) -> None:
         """Initialize the abstract data serializer."""
-        self.data: dict[str, T] = data
+        self.data: RecordDict[T] = data
 
     @abstractmethod
-    def moedl_to_dict(self) -> dict[str, Any]:
+    def moedl_to_dict(self) -> RecordDict[Any]:
         """
         Convert the data structure to a dictionary.
 
         Returns:
-            dict[str, Any]: A dictionary representation of the data structure.
+            RecordDict[Any]: A dictionary representation of the data structure.
 
         """
         raise NotImplementedError
 
     @abstractmethod
-    def dict_to_model(self) -> dict[str, T]:
+    def dict_to_model(self) -> RecordDict[T]:
         """
         Convert a dictionary to a data model.
 
         Returns:
-            dict[str, T]: A dictionary containing data models.
+            RecordDict[T]: A dictionary containing data models.
 
         """
         raise NotImplementedError
