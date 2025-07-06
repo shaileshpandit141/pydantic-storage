543600346df935d381c23ed51faf696b853cf8e0
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-06 20:28:43 +0530

    refactor: remove duplicate initializer.

--- src/json_file_storage/_services/_json_file_manager.py
+++ src/json_file_storage/_services/_json_file_manager.py
@@ -9,10 +9,6 @@ from json_file_storage.models.typed import FileDict, T
 class JsonFileManager(AbstractFileManager[T]):
     """A class for managing JSON file operations."""
 
-    def __init__(self, file_path: str) -> None:
-        """Initialize the JsonFileManager."""
-        self.file: Path = Path(file_path)
-
     def exists(self) -> bool:
         """
         Check if the JSON file exists.
