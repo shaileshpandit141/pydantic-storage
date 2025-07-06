4814852071865abddd5bcce4725d0abcb67a24b2
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-06 21:53:09 +0530

    feat: add pydantic model serializer features.

---
+++ src/json_file_storage/serializers/_pydantic_model_serializer.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+from json_file_storage._abstractions._abstract_pydantic_model_serializer import (
+    AbstractPydanticModelSerializer,
+)
+from json_file_storage.models.typed import RecordDict, T
+
+
+class PydanticModelSerializer(AbstractPydanticModelSerializer[T]):
+    """Concrete implementation of the abstract Pydantic model serializer."""
+
+    def model_to_dict(self) -> RecordDict[Any]:
+        """Convert the data structure to a dictionary."""
+        return {
+            "records": {
+                key: value.model_dump() for key, value in self.data["records"].items()
+            }
+        }
+
+    def dict_to_model(self) -> RecordDict[T]:
+        """Convert a dictionary to a data model."""
+        return {
+            "records": {
+                key: self.pydantic_model.model_validate(value)
+                for key, value in self.data["records"].items()
+            }
+        }
