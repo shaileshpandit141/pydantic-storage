b721741e52c49c239858441ea15ab59ecdc8e703
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-09 15:45:26 +0530

    feat: add pydantic storage model.

--- src/json_file_storage/models/pydantic/__init__.py
+++ src/json_file_storage/models/pydantic/__init__.py
@@ -1,9 +1,11 @@
 from ._models import (
+    Storage,
     T,
     Timestamp,
 )
 
 __all__: list[str] = [
+    "Storage",
     "T",
     "Timestamp",
 ]
--- src/json_file_storage/models/pydantic/_models.py
+++ src/json_file_storage/models/pydantic/_models.py
@@ -8,3 +8,8 @@ T = TypeVar("T", bound=BaseModel)
 class Timestamp(BaseModel):
     created_at: str = Field(..., description="Creation timestamp (ISO 8601)")
     updated_at: str = Field(..., description="Last update timestamp (ISO 8601)")
+
+
+class Storage(BaseModel):
+    type: str = Field(..., description="Storage backend type (e.g., local, s3)")
+    encryption: str = Field(..., description="Encryption method used (e.g., AES256)")
