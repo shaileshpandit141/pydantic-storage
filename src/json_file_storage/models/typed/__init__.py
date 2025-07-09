48741fdb53692ebc3a05360d4d49471ae8d4daef
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-09 15:25:25 +0530

    refactor: run code formatter.

--- src/json_file_storage/models/typed/__init__.py
+++ src/json_file_storage/models/typed/__init__.py
@@ -1,10 +1,4 @@
-from ._types import (
-    FileDict,
-    MetaDataConfigDict,
-    MetaDataDict,
-    RecordDict,
-    T,
-)
+from ._types import FileDict, MetaDataConfigDict, MetaDataDict, RecordDict, T
 
 __all__: list[str] = [
     "FileDict",
