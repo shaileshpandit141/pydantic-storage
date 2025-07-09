08e9a16fe7e290ff52ebc46f35a3dca14bef00e0
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-09 15:46:18 +0530

    feat: add pydantic base meta data  model.

--- src/json_file_storage/models/pydantic/__init__.py
+++ src/json_file_storage/models/pydantic/__init__.py
@@ -1,10 +1,12 @@
 from ._models import (
+    BaseMetaData,
     Storage,
     T,
     Timestamp,
 )
 
 __all__: list[str] = [
+    "BaseMetaData",
     "Storage",
     "T",
     "Timestamp",
--- src/json_file_storage/models/pydantic/_models.py
+++ src/json_file_storage/models/pydantic/_models.py
@@ -13,3 +13,9 @@ class Timestamp(BaseModel):
 class Storage(BaseModel):
     type: str = Field(..., description="Storage backend type (e.g., local, s3)")
     encryption: str = Field(..., description="Encryption method used (e.g., AES256)")
+
+
+class BaseMetaData(BaseModel):
+    version: str = Field(..., description="Schema or file version")
+    title: str = Field(..., description="Human-readable title of the file")
+    description: str = Field(..., description="Brief description of the file contents")
