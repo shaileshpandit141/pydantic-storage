d4bb6c3dd1bcdd5d17c29d5fb149a777bd5e8c67
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-11 22:45:24 +0530

    refactor: delete abstract pydantic model serializer file

--- src/json_file_storage/_abstractions/__init__.py
+++ src/json_file_storage/_abstractions/__init__.py
@@ -1,9 +1,7 @@
 from ._abstract_file_manager import AbstractFileManager
 from ._abstract_file_storage import AbstractFileStorage
-from ._abstract_pydantic_model_serializer import AbstractPydanticModelSerializer
 
 __all__: list[str] = [
     "AbstractFileManager",
     "AbstractFileStorage",
-    "AbstractPydanticModelSerializer",
 ]
