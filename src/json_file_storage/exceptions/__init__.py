3b216f27baeb99c4ae0ceb9004398f030dc5d30d
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-12 21:23:07 +0530

    refactor: delete unnecessary custom exception modeule

--- src/json_file_storage/exceptions/__init__.py
+++ src/json_file_storage/exceptions/__init__.py
@@ -1,11 +1,7 @@
 from ._duplicate_entry_error import DuplicateEntryError
-from ._json_decode_error import JSONDecodeError
 from ._validation_error import ValidationError
-from .json_encode_error import JSONEncodeError
 
 __all__: list[str] = [
     "DuplicateEntryError",
-    "JSONDecodeError",
-    "JSONEncodeError",
     "ValidationError",
 ]
