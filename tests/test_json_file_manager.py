d9f8488dca578ad45199ecdd36847bb98405e4ad
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-19 16:23:27 +0530

    test: add test case for read method

--- tests/test_json_file_manager.py
+++ tests/test_json_file_manager.py
@@ -1,21 +1,19 @@
 from src.json_file_storage._services._json_file_manager import JsonFileManager
 from pydantic import BaseModel
 import pytest as pyt
-
-
-# =====================
-# DUMMY PYDANTIC MODELS
-# =====================
-
-
-class User(BaseModel):
-    id: int
-    name: str
+from src.json_file_storage.models.pydantic import FileData
 
 
 # ====================
 # JSONFILEMANAGER TEST
 # ====================
+
+
+# DUMMY PYDANTIC MODELS
+# ---------------------
+class User(BaseModel):
+    id: int
+    name: str
 
 
 @pyt.fixture
@@ -36,3 +34,16 @@ def test_json_file_manager_instance_created(
 ) -> None:
     """Testing json file manager instance creation"""
     assert isinstance(manager, JsonFileManager)
+
+
+def test_read_method(
+    manager: JsonFileManager[User],
+) -> None:
+    """Testing read method"""
+
+    data: FileData[User] = manager.read()
+
+    assert isinstance(data.metadata.version, str)
+    assert isinstance(data.metadata.title, str)
+    assert isinstance(data.metadata.description, str)
+    assert isinstance(data.records, dict)
