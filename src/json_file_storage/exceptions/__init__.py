29a08aee0055a7fa5ac394946216ca41cd3e2929
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-23 17:37:45 +0530

    refactor: delete unused exception

--- src/json_file_storage/exceptions/__init__.py
+++ src/json_file_storage/exceptions/__init__.py
@@ -1,7 +1,5 @@
 from ._duplicate_entry_error import DuplicateEntryError
-from ._validation_error import ValidationError
 
 __all__: list[str] = [
     "DuplicateEntryError",
-    "ValidationError",
 ]
