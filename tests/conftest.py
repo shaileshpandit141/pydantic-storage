3be35c80906e7b6cd376d3c0651cbd72afede84b
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-24 18:09:31 +0530

    test: create storage fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 from pytest import fixture
 from pydantic import BaseModel
 
-from json_file_storage._services import JsonFileManager
+from json_file_storage._services import JsonFileManager, JsonFileStorage
+from .test_helpers import FakeUser
 
 
 # Dummy User model for testing
@@ -23,4 +24,18 @@ def manager() -> JsonFileManager[User]:
             "title": "User records",
             "description": "User record descriptions",
         },
+    )
+
+
+@fixture(scope="module")
+def storage() -> JsonFileStorage[FakeUser]:
+    return JsonFileStorage[FakeUser](
+        file_path="./users.json",
+        model_class=User,
+        metadata={
+            "version": "1.0.0",
+            "title": "User records",
+            "description": "User record descriptions",
+        },
+        unique_fields=["id", "email"],
     )
