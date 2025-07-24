d10a97c6d90248966f5019dd7374b1400adb8213
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-24 18:14:47 +0530

    test: add test case for json file initialization

---
+++ tests/test_json_file_storage.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from json_file_storage import JsonFileStorage
+from .test_helpers import FakeUser
+
+
+def test_json_file_storage_initialization(storage: JsonFileStorage[FakeUser]) -> None:
+    """Test the initialization of JsonFileStorage."""
+    assert isinstance(storage.file_path, Path)
+    assert isinstance(storage.model_class, FakeUser)
+    assert isinstance(storage.unique_fields, list)
