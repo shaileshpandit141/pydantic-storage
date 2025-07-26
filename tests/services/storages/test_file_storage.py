f0256d3a45f797725d213b341af97258fb0e6364
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-26 18:57:37 +0530

    test: add test case for add method

--- tests/services/storages/test_file_storage.py
+++ tests/services/storages/test_file_storage.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+from pydantic import ValidationError
+from pytest import raises
+
 from pydantic_storage._services import FileStorage
 from tests.mocks.models import FakeUser
 
@@ -16,3 +19,32 @@ def test_all_method(storage: FileStorage[FakeUser]) -> None:
     items = storage.all()
     assert isinstance(items, list)
     assert all(isinstance(item, FakeUser) for item in items)
+
+
+def test_get_method(storage: FileStorage[FakeUser]) -> None:
+    """Test the get method of FileStorage."""
+    user = storage.get(name="John Doe")
+    assert isinstance(user, FakeUser)
+    assert user.name == "John Doe"
+
+    # Test with multiple key-value pair
+    user = storage.get(
+        name="Alice",
+        email="alice@gmail.com",
+    )
+    assert isinstance(user, FakeUser)
+
+    # Test with non-existing multiple key-value pair
+    user = storage.get(
+        name="Alice",
+        email="shailesh@gmail.com",
+    )
+    assert isinstance(user, FakeUser)
+
+    # Test with non-existing key-value pair
+    user = storage.get(name="Non Existent")
+    assert user is None
+
+    # Test with invalid field
+    with raises(ValidationError):
+        storage.get(age=25)
