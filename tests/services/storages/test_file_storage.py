d550801df03829e00eec2967cfed7fba458c296a
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-07-26 18:39:02 +0530

    test: add test case for all method

--- tests/services/storages/test_file_storage.py
+++ tests/services/storages/test_file_storage.py
@@ -9,3 +9,10 @@ def test_json_file_storage_initialization(storage: FileStorage[FakeUser]) -> Non
     assert isinstance(storage.file_path, Path)
     assert isinstance(storage.model_class, FakeUser)
     assert isinstance(storage.unique_fields, list)
+
+
+def test_all_method(storage: FileStorage[FakeUser]) -> None:
+    """Test the all method of FileStorage."""
+    items = storage.all()
+    assert isinstance(items, list)
+    assert all(isinstance(item, FakeUser) for item in items)
