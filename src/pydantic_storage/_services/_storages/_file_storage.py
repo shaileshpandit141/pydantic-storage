2cdf788fdabae04657c921e0dc5a4142fd059db2
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-12 09:02:45 +0530

    feat: buid logic of metadata property

--- src/pydantic_storage/_services/_storages/_file_storage.py
+++ src/pydantic_storage/_services/_storages/_file_storage.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from pydantic_storage._services import FileManager
 from pydantic_storage.abstractions import BaseStorage
 from pydantic_storage.abstractions._managers._base_manager import BaseManager
+from pydantic_storage.models import MetaData
 from pydantic_storage.types._generic_types import T
 from pydantic_storage.types._model_dict_types import MetaDataDict
 
@@ -27,3 +28,8 @@ class FileStorage(BaseStorage[T]):
     def model(self) -> type[T]:
         """Return Current Model"""
         return self._model
+
+    @property
+    def metadata(self) -> MetaData:
+        """Return Current Model"""
+        return self.manager.metadata
