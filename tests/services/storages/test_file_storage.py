3122b95498c4a6256e3cdd45702e61091ad5cdfd
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-12 18:56:52 +0530

    test: add test case for create data

--- tests/services/storages/test_file_storage.py
+++ tests/services/storages/test_file_storage.py
@@ -16,3 +16,16 @@ def test_get_all_data(
 ) -> None:
     """Test get all data from file"""
     assert isinstance(file_storage.all(), list)
+
+
+def test_create_data(
+    file_storage: FileStorage[FakeUser],
+) -> None:
+    """Test create data"""
+    data: list[FakeUser] = [
+        FakeUser(name="ashis", email="ashis@gmail.com"),
+    ]
+    created_data = file_storage.create(data=data)
+
+    if len(created_data) > 0:
+        assert data[0].name == created_data[0].name
